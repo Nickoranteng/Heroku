e14af6401eb24e40f19e2f63727517aa70626d7d
Nickoranteng <nickoranteng@gmail.com>
2021-01-23 22:51:02 -0500

    heroku 1

---
+++ Solved/config.py
@@ -0,0 +1,5 @@
+remote_db_endpoint = 'databootcamp.chuz4gaw50km.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = '1001_magnita'
+remote_db_name = 'gwsis'
