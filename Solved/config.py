8cc6cbf4dcbeb1e8896c5b7add22fb333ed50107
Nickoranteng <nickoranteng@gmail.com>
2021-01-23 22:53:35 -0500

    hero

--- Solved/config.py
+++
@@ -1,5 +0,0 @@
-remote_db_endpoint = 'databootcamp.chuz4gaw50km.us-east-2.rds.amazonaws.com'
-remote_db_port = '3306'
-remote_db_user = 'admin'
-remote_db_pwd = '1001_magnita'
-remote_db_name = 'gwsis'
